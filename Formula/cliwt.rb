54cfa864af609d43808e85d6e9f39dc28b24fc18
mininit <50385867+mininit@users.noreply.github.com>
2025-11-14 22:10:08 +1000

    chore: new brew build/install process

--- Formula/cliwt.rb
+++ Formula/cliwt.rb
@@ -1,26 +1,15 @@
 class Cliwt < Formula
   desc "Cute terminal waifu tamagotchi for you"
   homepage "https://github.com/HenryLoM/CliWaifuTamagotchi"
-  url "https://github.com/HenryLoM/CliWaifuTamagotchi/releases/download/v1.0.0/cliwt-v1.0.0.tar.gz"
+  url "https://github.com/HenryLoM/CliWaifuTamagotchi/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "589175e2a46a00d85a4bf2811ae5aed54ad25e5241cd2c7e6a006eede48e8d5b"
-  version "1.0.0"
+  license "GPL-3.0-only"
+
+  head "https://github.com/HenryLoM/CliWaifuTamagotchi.git", branch: "main"
+
+  depends_on "go" => :build
 
   def install
-    # Install binary and rename it to avoid conflict with wrapper
-    bin.install "cliwt" => "cliwt.real"
-
-    # Install asset directories
-    prefix.install "assets", "ascii-arts", "utils"
-
-    # Wrapper script to run binary from the prefix directory
-    (bin/"cliwt").write <<~EOS
-      #!/bin/bash
-      cd "#{prefix}" || exit 1
-      exec "#{bin}/cliwt.real" "$@"
-    EOS
-  end
-
-  test do
-    system "#{bin}/cliwt", "--help"
+    system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 end
